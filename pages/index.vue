7be629a515a7b737a02de2c90e8e7ef33c44173b
Phillip Curl <phillipc@ohanet.org>
2017-11-16 16:02:39 -0500

    Adding stats to summary

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,35 @@
     <div v-if="!isLoading && formSubmitted" class="pt5">
       <button @click="newSearch" class="f6 f5-l button-reset dib pa3 tc bn bg-animate bg-blue hover-bg-navy white pointer br2" type="button">New Search</button>          
       <br>
+      <article class="pa3 pa5-ns" data-name="slab-stat-small">
+        <h3 class="f6 ttu tracked">Stats</h3>
+        <div class="cf">
+          <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+            <dd class="f6 fw4 ml0">Total Words</dd>
+            <dd class="f3 fw6 ml0">{{ linkData.words }}</dd>
+          </dl>
+          <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+            <dd class="f6 fw4 ml0">Minutes</dd>
+            <dd class="f3 fw6 ml0">{{ linkData.minutes }}</dd>
+          </dl>
+          <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+            <dd class="f6 fw4 ml0">Difficulty</dd>
+            <dd class="f3 fw6 ml0">{{ linkData.difficulty }}</dd>
+          </dl>
+          <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+            <dd class="f6 fw4 ml0">Sentiment</dd>
+            <dd class="f3 fw6 ml0">{{ linkData.sentiment }}</dd>
+          </dl>
+          <!-- <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+            <dd class="f6 fw4 ml0">Favorite Cat</dd>
+            <dd class="f3 fw6 ml0">All of Them</dd>
+          </dl>
+          <dl class="fl fn-l w-50 dib-l w-auto-l lh-title">
+            <dd class="f6 fw4 ml0">App Downloads</dd>
+            <dd class="f3 fw6 ml0">1,200</dd>
+          </dl> -->
+        </div>
+      </article>
       <code v-if="formSubmitted"><pre style="overflow: hidden; word-wrap: break-word;">{{linkData}}</pre></code>
     </div> 
   </section>
